c8ad9559cb63c615602181e8eb816f763d45dfd9
dccruz <dccoder@icloud.com>
2017-06-13 09:40:41 -0700

    Zodiac Web App

---
+++ js/main.js
@@ -0,0 +1,200 @@
+var zodiac = [
+{	sign: "aquarius",
+	element: "----", 
+	quality: "-----",
+	color: "Light-Blue & Silver.", 
+	day: "Saturday.",
+	compatibility: "Leo & Sagittarius.",
+	numbers: "4, 7, 11, 22, & 29.",
+	strengths: "Progressive, original, independent, humanitarian",
+	weakness: "Runs from emotional expression, temperamental, uncompromising, aloof",
+	likes: "Fun with friends, helping others, fighting for causes, intellectual conversation, a good listener",
+	dislikes: "Limitations, broken promises, being lonely, dull or boring situations, people who disagree with them",
+	love: "Intellectual stimulation is by far the greatest aphrodisiac for Aquarius. There's nothing that can attract an Aquarius more than an interesting conversation with a person. Openness, communication, imagination and willingness to risk are the qualities that fit well in the perspective of life of this zodiac sign. ",
+	image: "img/aquarius2.png"
+	},
+{	sign: "aries",
+	element: "Fire.", 
+	quality: "Cardinal.",
+	color: "Red.", 
+	day: "Tuesday.",
+	compatibility: "Libra & Leo.",
+	numbers: " 1, 8, & 17.",
+	strengths: "Courageous, determined, confident, enthusiastic, optimistic, honest, passionate",
+	weakness: "Impatient, moody, short-tempered, impulsive, aggressive",
+	likes: "Comfortable clothes, taking on leadership roles, physical challenges, individual sports",
+	dislikes: "Inactivity, delays, work that does not use one's talents",
+	love: "Aries is a fire sign with the need to take initiative when it comes to romance. When they fall in love, they will express their feelings to the person they are in love with, without even giving it a considerable thought.",
+	image: "img/aries.png"
+	},
+{	sign: "taurus",
+	element: "Earth.", 
+	quality: "Fixed.",
+	color: "Green & Pink.", 
+	day: " Friday & Monday.",
+	compatibility: "Scorpio & Cancer.",
+	numbers: "2, 6, 9, 12, & 24.",
+	strengths: "Reliable, patient, practical, devoted, responsible, stable",
+	weakness: "Stubborn, possessive, uncompromising",
+	likes: "Gardening, cooking, music, romance, high quality clothes, working with hands",
+	dislikes: "Sudden changes, complications, insecurity of any kind, synthetic fabrics",
+	love: "One always has to be prepared to have patience for a Taurus lover. They are extremely sensual, touch, smell and all pleasurable senses being extremely important to them, but they also need time to create a safe environment and relax in their encounters.",
+	image: "img/taurus.png"
+	},
+{	sign: "gemini",
+	element: "Air.",
+	quality: "Mutable.",
+	color: " Light-Green & Yellow.", 
+	day: "Wednesday.",
+	compatibility: "Sagittarius & Aquarius.",
+	numbers: "5, 7, 14, & 23.",
+	strengths: " Gentle, affectionate, curious, adaptable, ability to learn quickly and exchange ideas",
+	weakness: "Nervous, inconsistent, indecisive",
+	likes: "Music, books, magazines, chats with nearly anyone, short trips around the town",
+	dislikes: "Being alone, being confined, repetition and routine",
+	love: "Fun and always ready for an intellectual challenge, Gemini sees love first through communication and verbal contact, and find it as important as physical contact with their partner. When these two combine, obstacles all seem to fade.",
+	image: "img/gemini.png" 
+	},
+{	sign: "libra",
+	element: "Fire.", 
+	quality: "Cardinal.",
+	color: "Red.", 
+	day: "Tuesday.",
+	compatibility: "Libra & Leo",
+	numbers: " 1, 8, & 17.",
+	strengths: "Courageous, determined, confident, enthusiastic, optimistic, honest, passionate",
+	weakness: "Impatient, moody, short-tempered, impulsive, aggressive",
+	likes: "Comfortable clothes, taking on leadership roles, physical challenges, individual sports",
+	dislikes: "Inactivity, delays, work that does not use one's talents",
+	love: "Aries is a fire sign with the need to take initiative when it comes to romance. When they fall in love, they will express their feelings to the person they are in love with, without even giving it a considerable thought.",
+	image: "img/libra.png"
+	},
+{	sign: "scorpio",
+	element: "Water.", 
+	quality: "Fixed.",
+	color: "Scarlet, Red, & Rust.", 
+	day: "Tuesday.",
+	compatibility: "Taurus & Cancer.",
+	numbers: " 8, 11, 18, & 22.",
+	strengths: "Resourceful, brave, passionate, stubborn, a true friend",
+	weakness: "Distrusting, jealous, secretive, violent",
+	likes: "Truth, facts, being right, longtime friends, teasing, a grand passion",
+	dislikes: "Dishonesty, revealing secrets, passive people",
+	love:  "Scorpio is the most sensual sign of the zodiac. Scorpios are extremely passionate and intimacy is very important to them.",
+	image: "img/scorpio.png"
+	},
+{	sign: "sagittarius",
+	element: " Fire.", 
+	quality: " Mutable.",
+	color: " Blue.", 
+	day: "Thursday.",
+	compatibility: "Gemini & Aries.",
+	numbers: "3, 7, 9, 12, & 21.",
+	strengths: "Generous, idealistic, great sense of humor",
+	weakness: "Promises more than can deliver, very impatient, will say anything no matter how undiplomatic",
+	likes: "Freedom, travel, philosophy, being outdoors",
+	dislikes: "Clingy people, being constrained, off-the-wall theories, details",
+	love: "People born under the sign of Sagittarius are very playful and humorous, which means that they will enjoy having fun with their partners. Partners who are equally open, will certainly suit the passionate, expressive Sagittarius who is willing to try almost anything.",
+	image: "img/sagittarius.png"
+	},
+{	sign: "cancer",
+	element: "Water.", 
+	quality: "Cardinal.",
+	color: "White.", 
+	day: "Monday & Thursday.",
+	compatibility: "Capricorn & Taurus.",
+	numbers: " 2, 3, 15, & 20.",
+	strengths: "Tenacious, highly imaginative, loyal, emotional, sympathetic, persuasive",
+	weakness: "Moody, pessimistic, suspicious, manipulative, insecure",
+	likes: "Art, home-based hobbies, relaxing near or in water, helping loved ones, a good meal with friends",
+	dislikes: "Strangers, any criticism of Mom, revealing of personal life",
+	love: "Cancer is a very emotional sign, and feelings are the most important thing in their relationships. Gentle and caring, they will show their sensibility to the world without even thinking they might get hurt. For partners, they always choose a person who is able to understand them through non-verbal, silent contact, and a shared daily routine, and their affection won't last long with superficial, flaky or unreliable partners. ",
+	image: "img/cancer.png"
+	},
+{	sign: "capricorn",
+	element: "Earth.", 
+	quality: "Cardinal.",
+	color: "Brown & Black", 
+	day: "Saturday.",
+	compatibility: " Taurus & Cancer.",
+	numbers: "4, 8, 13, & 22.",
+	strengths: "Responsible, disciplined, self-control, good managers",
+	weakness: "Know-it-all, unforgiving, condescending, expecting the worst",
+	likes: "Family, tradition, music, understated status, quality craftsmanship",
+	dislikes: "Almost everything at some point",
+	love: " is not easy to win over the attention and the heart of a Capricorn, but once their walls break and their heart melts they stay committed for a lifetime.",
+	image: "img/capricorn.png"
+	},
+{	sign: "leo",
+	element: "Fire.", 
+	quality: "Fixed.",
+	color: "Gold, Yellow, & Orange.", 
+	day: "Sunday.",
+	compatibility: "Aquarius & Gemini.",
+	numbers: "1, 3, 10, & 19.",
+	strengths: "Creative, passionate, generous, warm-hearted, cheerful, humorous",
+	weakness: "Arrogant, stubborn, self-centered, lazy, inflexible",
+	likes: "Theater, taking holidays, being admired, expensive things, bright colors, fun with friends",
+	dislikes: "Being ignored, facing difficult reality, not being treated like a king or queen",
+	love: "This Fire sign is passionate and sincere and its representatives show their feelings with ease and clarity. When in love, they are fun, loyal, respectful and very generous towards their loved one. ",
+	image: "img/leo.png"
+	},
+{	sign: "virgo",
+	element: " Earth.", 
+	quality: "Mutable.",
+	color: "Grey, Beige, & Pale-Yellow.", 
+	day: "Wednesday.",
+	compatibility: "Pisces & Cancer.",
+	numbers: "5, 14, 15, 23, & 32.",
+	strengths: "Loyal, analytical, kind, hardworking, practical",
+	weakness: "Shyness, worry, overly critical of self and others, all work and no play",
+	likes: "Animals, healthy food, books, nature, cleanliness",
+	dislikes: "Rudeness, asking for help, taking center stage",
+	love: "The sign of Virgo leads Venus to its tragic fall and speaks of one’s inability to feel worthy, beautiful, or lovable. ",	
+	image: "img/virgo.png"
+	},
+{	sign: "pisces",
+	element: "Water.", 
+	quality: "Mutable.",
+	color: "Mauve, Lilac, Purple, Violet, & Sea green.", 
+	day: "Thursday.",
+	compatibility: "Virgo & Taurus.",
+	numbers: "3, 9, 12, 15, 18, & 24.",
+	strengths: "Compassionate, artistic, intuitive, gentle, wise, musical",
+	weakness: " Fearful, overly trusting, sad, desire to escape reality, can be a victim or a martyr",
+	likes: "Being alone, sleeping, music, romance, visual media, swimming, spiritual themes",
+	dislikes: " Know-it-all, being criticized, the past coming back to haunt, cruelty of any kind",
+	love: "Deep in their hearts, Pisces-born are incorrigible romantics. They are very loyal, gentle and unconditionally generous to their partners.",
+	image: "img/pisces.png"
+	}
+
+]
+
+
+
+function horoscope() {
+	console.log("functionFire")
+	var sign = document.getElementById("sign").value.toLowerCase()
+	console.log(sign)
+	for(i = 0; i < zodiac.length; i++) {
+		if(sign === zodiac[i].sign) {
+			document.getElementById("yourSign").innerHTML = zodiac[i].sign
+			document.getElementById("element").textContent = "Your element is " + zodiac[i].element
+			document.getElementById("quality").textContent = "Your quality is " + zodiac[i].quality
+			document.getElementById("color").textContent = "The colors that are good for you are " + zodiac[i].color
+			document.getElementById("day").textContent = "Your lucky day is " + zodiac[i].day
+			document.getElementById("numbers").textContent = "Your lucky numbers are " + zodiac[i].numbers
+			document.getElementById("strengths").textContent = "Your strongest attributes are: " + zodiac[i].strengths
+			document.getElementById("weakness").textContent = "Your biggest oppurtunities are: " + zodiac[i].weakness
+			document.getElementById("likes").textContent = "Your likes are: " + zodiac[i].likes
+			document.getElementById("dislikes").textContent = "Your dislikes are: " + zodiac[i].dislikes
+			document.getElementById("love").textContent = "When it coses to love: " + zodiac[i].love
+			document.getElementById("icon").src = zodiac[i].image
+			return
+		} else {
+			document.getElementById("yourSign").innerHTML = "That's not one of the signs. Try again!"
+			document.getElementById("sign").innerHTML = ""
+			document.getElementById("icon").src = ""
+		}
+	}
+}
---
+++ read.md
@@ -0,0 +1,7 @@
+Create a Zodiac JavaScript App, that shows the user some zodiac information based on their sign.<br>
+	-Create array of objects with some zodiac information.<br>
+	-Create a function to get the user submitted sign.<br>
+	-Loop through sign data to find a match.<br>
+	-Display the data for that zodiac sign.<br>
+	-Add additional features to your program.<br>
+	-Add styles to your program.<br>
